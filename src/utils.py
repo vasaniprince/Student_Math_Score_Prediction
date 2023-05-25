99093e7ddbac8f21822356e0e4a6b824db74380e
vasani prince <vasaniprince999@gmail.com>
2023-05-25 14:49:06 +0530

    Hyper parameter training

--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ import dill
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -23,12 +24,17 @@ def save_object(file_path,obj):
     
 
 
-def evaluate_model(x_train,y_train,x_test,y_test,models):
+def evaluate_model(x_train,y_train,x_test,y_test,models,param):
     try:
         report={}
         for i in range(len(list(models))):
-            model=list(models.values())[i]    
+            model=list(models.values())[i]  
+            para=param[list(models.keys())[i]]
 
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(x_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(x_train,y_train) # Train Model
 
             y_train_pred=model.predict(x_train)
